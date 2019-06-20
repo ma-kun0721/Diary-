3c9b1bb8bece421f5b731dda76d6317b0c3d8812
古市仁彦 <zhengyangushi@gmail.com>
2019-06-20 15:15:10 +0800

    一覧機能を追加
    一覧機能追加

---
+++ app/Http/Controllers/DiaryController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use App\Diary;
+use App\Http\Requests\CreateDiary;
+
+class DiaryController extends Controller
+{
+    public function index()
+
+    {
+        $diaries = Diary::all();
+        return view('diaries.index',['diaries' => $diaries]);
+    }
+
+      public function create()
+    {
+        // views/diaries/create.blade.phpを表示する
+        return view('diaries.create');
+    }
+
+    public function store(CreateDiary $request)
+{
+    $diary = new Diary(); //Diaryモデルをインスタンス化
+
+    $diary->title = $request->title; //画面で入力されたタイトルを代入
+    $diary->body = $request->body; //画面で入力された本文を代入
+    $diary->save(); //DBに保存
+
+    return redirect()->route('diary.index'); //一覧ページにリダイレクト
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'DiaryController@index')->name('diary.index'); //追加
+
+Route::get('diary/create', 'DiaryController@create')->name('diary.create'); // 投稿画面
+Route::post('diary/create', 'DiaryController@store')->name('diary.create'); // 保存処理
+
+
+
